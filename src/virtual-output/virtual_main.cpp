612f25cab57cbc202505b1182d69f439902c5d0b
jgarcie <jordan.garcie@gmail.com>
2021-05-27 22:21:03 -0400

    changing virtual cam name + adding back virtual filter

--- src/virtual-output/virtual_main.cpp
+++ src/virtual-output/virtual_main.cpp
@@ -1,11 +1,11 @@
 #include <obs-module.h>
 #include "virtual_output.h"
+#include "virtual_filter.h"
 
 #ifdef QT_DISABLED
 #include <QMainWindow>
 #include <QAction>
 #include <obs-frontend-api.h>
-#include "virtual_filter.h"
 #include "virtual_properties.h"
 
 VirtualProperties* virtual_prop;
@@ -17,9 +17,9 @@ OBS_MODULE_USE_DEFAULT_LOCALE("obs-virtual_output", "en-US")
 bool obs_module_load(void)
 {
 	virtual_output_init();
-#ifdef QT_DISABLED
 	virtual_filter_init();
 
+#ifdef QT_DISABLED
 	QMainWindow* main_window = (QMainWindow*)obs_frontend_get_main_window();
 	QAction *action = (QAction*)obs_frontend_add_tools_menu_qaction(
 		obs_module_text("VirtualCam"));
